ab39b38c3f5211b3beb8d69babe2bd8001457d5e
Aditya Chauhan <aditya.chauhan00018@gmail.com>
2025-08-21 11:53:43 +0530

    Initial commit

---
+++ src/main/resources/data1.sql
@@ -0,0 +1,4 @@
+INSERT INTO product(name,description,brand,price,category,release_date,available,quantity) VALUES ('Tata Nexon', 'A Compact SUV with  excellent safety features and performance.', 'Tata Motors', '750000.00', 'Cars', '2025-08-12', true, 50),
+('Tata Harrier', 'A PremiumSUV with  excellent safety features and performance.', 'Tata Motors', '1050000.00', 'Cars', '2025-08-12', true, 10),
+('Tata Punch', 'A Compact SUV with  excellent safety features and performance.', 'Tata Motors', '750000.00', 'Cars', '2025-08-12', true, 70),
+('Swift', 'A Compact Sedan with  excellent performance.', 'Maruti Suzuki', '650000.00', 'Cars', '2025-08-12', true, 100);
